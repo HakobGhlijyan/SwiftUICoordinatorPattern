f3e75cbb7ab7afa00d201e77f2b42df41716a6aa
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-06-11 16:40:56 +0400

    Initial Commit

---
+++ SwiftUICoordinatorPattern/SwiftUICoordinatorPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoordinatorPatternApp.swift
+//  SwiftUICoordinatorPattern
+//
+//  Created by Hakob Ghlijyan on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoordinatorPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
